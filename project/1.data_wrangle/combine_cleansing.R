47c7d55aaad1ec84262d58391f66004ec7a51055
Seunghun_Yu <rsh1502@naver.com>
2018-06-05 22:00:18 +0900

    Delete combine_cleansing.R

--- project/1.data_wrangle/combine_cleansing.R
+++
@@ -1,8 +0,0 @@
-pkgs <- c('dplyr','stringr','rjson','progress')
-sapply(pkgs,require,character.only = TRUE)
-
-setwd('C:/Users/Seung Hun/Desktop/dataming_project/data')
- 
-new <- read.csv('new.csv')
-old <- read.csv('old.csv')
-
